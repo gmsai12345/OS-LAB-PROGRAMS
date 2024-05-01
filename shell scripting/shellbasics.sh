0bf0481b68662148799dcda8a458c6802231e2a2
Sai Dhakshan Y <112693557+gmsai12345@users.noreply.github.com>
2024-05-01 16:54:24 +0530

    shell basics

---
+++ shell scripting/shellbasics.sh
@@ -0,0 +1,18 @@
+NAME="SAI"
+echo $NAME
+array_name[index]=value
+array_name[0]="Zara"
+array_name[1]="Qadir"
+array_name[2]="Mahnaz"
+array_name[3]="Ayan"
+array_name[4]="Daisy"
+echo ${array_name[1]}
+a=20
+b=20
+# Corrected if conditions
+if [ $a -eq $b ]
+then
+    echo "a is equal to b"
+else
+    echo "a is not equal to b"
+fi
